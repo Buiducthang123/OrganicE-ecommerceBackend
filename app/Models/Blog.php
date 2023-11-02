45c1df615a0a90357defcb81b8530d2743327a62
Thang Dep Zai <winnerbui0803@gmail.com>
2023-11-02 21:03:19 +0700

    fix

--- app/Models/Blog.php
+++ app/Models/Blog.php
@@ -13,6 +13,6 @@ class Blog extends Model
     }
     
     public function comments() {
-        return $this->belongsToMany(User::class,"comments");
+        return $this->belongsToMany(User::class,"comments")->withPivot('content');
     }
 }
